5f6955248730f419ba5d2cb791a0a897238af50e
Davide Garberi <dade.garberi@gmail.com>
2018-12-30 12:16:00 +0100

    Moto changes import
    * Just an import of moto edits on top of LA.UM.7.6.r1-02500-89xx.0
    * Commits history (way too long to paste it here)
    	https://del.dog/pojegutoxa.sql

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -823,10 +823,6 @@ static int msm_compr_send_media_format_block(struct snd_compr_stream *cstream,
 		}
 
 		switch (prtd->codec_param.codec.format) {
-		case SNDRV_PCM_FORMAT_S32_LE:
-			bit_width = 32;
-			sample_word_size = 32;
-			break;
 		case SNDRV_PCM_FORMAT_S24_LE:
 			bit_width = 24;
 			sample_word_size = 32;
@@ -841,10 +837,7 @@ static int msm_compr_send_media_format_block(struct snd_compr_stream *cstream,
 			sample_word_size = 16;
 			break;
 		}
-
-		switch (q6core_get_avs_version()) {
-		case (Q6_SUBSYS_AVS2_7):
-			ret = q6asm_media_format_block_pcm_format_support_v3(
+		ret = q6asm_media_format_block_pcm_format_support_v3(
 							prtd->audio_client,
 							prtd->sample_rate,
 							prtd->num_channels,
@@ -852,24 +845,6 @@ static int msm_compr_send_media_format_block(struct snd_compr_stream *cstream,
 							use_default_chmap,
 							chmap,
 							sample_word_size);
-			break;
-		case (Q6_SUBSYS_AVS2_8):
-			ret = q6asm_media_format_block_pcm_format_support_v4(
-							prtd->audio_client,
-							prtd->sample_rate,
-							prtd->num_channels,
-							bit_width, stream_id,
-							use_default_chmap,
-							chmap,
-							sample_word_size,
-							ASM_LITTLE_ENDIAN,
-							DEFAULT_QF);
-			break;
-		case (Q6_SUBSYS_INVALID):
-		default:
-			pr_err("%s: INVALID AVS IMAGE\n", __func__);
-			break;
-		}
 		if (ret < 0)
 			pr_err("%s: CMD Format block failed\n", __func__);
 
@@ -1143,25 +1118,10 @@ static int msm_compr_configure_dsp_for_playback
 	} else {
 		pr_debug("%s: stream_id %d bits_per_sample %d\n",
 				__func__, ac->stream_id, bits_per_sample);
-
-		switch (q6core_get_avs_version()) {
-		case (Q6_SUBSYS_AVS2_7):
-			ret = q6asm_stream_open_write_v3(ac,
+		ret = q6asm_stream_open_write_v3(ac,
 				prtd->codec, bits_per_sample,
 				ac->stream_id,
 				prtd->gapless_state.use_dsp_gapless_mode);
-			break;
-		case (Q6_SUBSYS_AVS2_8):
-			ret = q6asm_stream_open_write_v4(ac,
-				prtd->codec, bits_per_sample,
-				ac->stream_id,
-				prtd->gapless_state.use_dsp_gapless_mode);
-			break;
-		case (Q6_SUBSYS_INVALID):
-		default:
-			pr_err("%s: INVALID AVS IMAGE\n", __func__);
-			break;
-		}
 		if (ret < 0) {
 			pr_err("%s:ASM open write err[%d] for compr type[%d]\n",
 				__func__, ret, prtd->compr_passthr);
@@ -2394,25 +2354,10 @@ static int msm_compr_trigger(struct snd_compr_stream *cstream, int cmd)
 
 		pr_debug("%s: open_write stream_id %d bits_per_sample %d",
 				__func__, stream_id, bits_per_sample);
-
-		switch (q6core_get_avs_version()) {
-		case (Q6_SUBSYS_AVS2_7):
-			rc = q6asm_stream_open_write_v3(ac,
+		rc = q6asm_stream_open_write_v3(prtd->audio_client,
 				prtd->codec, bits_per_sample,
 				stream_id,
 				prtd->gapless_state.use_dsp_gapless_mode);
-			break;
-		case (Q6_SUBSYS_AVS2_8):
-			rc = q6asm_stream_open_write_v4(ac,
-				prtd->codec, bits_per_sample,
-				stream_id,
-				prtd->gapless_state.use_dsp_gapless_mode);
-			break;
-		case (Q6_SUBSYS_INVALID):
-		default:
-			pr_err("%s: INVALID AVS IMAGE\n", __func__);
-			break;
-		}
 		if (rc < 0) {
 			pr_err("%s: Session out open failed for gapless\n",
 				 __func__);
